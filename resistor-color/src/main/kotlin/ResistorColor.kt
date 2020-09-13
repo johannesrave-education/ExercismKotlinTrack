2034335d212bad33149d98610ad658444c29adca
cutmeat <johannes.rave@gmail.com>
2020-09-13 19:31:58 +0200

    ResistorColor solution shortened ResistorColorDuo solution added

--- resistor-color/src/main/kotlin/ResistorColor.kt
+++ resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,5 @@
 object ResistorColor {
+    fun colorCode(color: String) = colors().indexOf(color)
 
-    fun colorCode(color: String): Int {
-        return colors().indexOf(color)
-    }
-
-    fun colors(): List<String> {
-        return listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
-    }
-
+    fun colors() = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
 }
